f09cbb2a6b01f0a84bca980d17aadd3e8dc21520
Daniel <fagundes.daniel191@gmail.com>
2023-05-03 14:41:25 -0300

    mm.py

--- mm.py
+++ mm.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 functions_list = [2, 3, 4, 5, 6, 7, 8]  # functions to be tested
 f_index = 0
-dims = [2, 10]
+dims = [10, 2]
 d_index = 0
-budget_multiplier = 10000
-n_runs = 1
+budget_multiplier = 1000 # 10000
+n_runs = 10 # 51
 
 def alternate_dimensions():
     global d_index
@@ -17,7 +17,7 @@ def alternate_dimensions():
 
 
 def test_fun(x):
-    x = np.array(x).reshape(2, dims[d_index])
+    x = np.array(x).reshape(1, dims[d_index])
     i = functions_list[f_index]
     for xi in enumerate(x):
         f = functions.all_functions[i](x)
@@ -28,31 +28,34 @@ if __name__ == '__main__':
 
     for f_index in range(len(functions_list)):
         for d_index in range(len(dims)):
-            print("\nFunction: f", functions_list[f_index])
+            print("\nFunction: f", (functions_list[f_index] + 1))
 
-            size_problem = int(2 * dims[d_index])
-            problem = Problem(size_problem, 2)
+            size_problem = int(dims[d_index])
+            problem = Problem(size_problem, 1)
             problem.types[:] = Real(-100, 100)
             problem.function = test_fun
             problem.directions[:] = Problem.MINIMIZE
             algorithm = NSGAII
 
             solutions_list = []
-            vetorY = []
+            fx_values = []
             for i in range(n_runs):
-                value = experiment(algorithm, problem, nfe=(10 * budget_multiplier), seeds=1)
+                value = experiment(algorithm, problem, nfe=(dims[d_index] * budget_multiplier), seeds=1)
                 solutions_list.append(value.get("NSGAII").get("Problem")[0][i].variables)
+                f = functions.all_functions[functions_list[f_index]] # encontrando o endereco da funcao
+                m = np.array(solutions_list[i]).reshape(5, -1) # transformando a lista 1x5 em matriz 5x1, apenas um teste
+                fx_values.append(f(m)) # chamando funcao f com a matriz m
 
-                for j in range(len(solutions_list[i])):
-                    vetorY.append((solutions_list[i][j]))
 
-            maior = max(vetorY)
-            menor = min(vetorY)
+
+
+            maior = max(fx_values)
+            menor = min(fx_values)
 
             # mean and std from lista
-            mean = np.mean(vetorY, axis=0)
-            median = np.median(vetorY, axis=0)
-            std = np.std(vetorY, axis=0)
+            mean = np.mean(fx_values, axis=0)
+            median = np.median(fx_values, axis=0)
+            std = np.std(fx_values, axis=0)
             print("D =" + str(dims[d_index]))
             print("media: ", mean)
             print("mediana: ", median)
