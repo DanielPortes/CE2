465bd39b77bb5853bf9c463e65917439ffb65126
Daniel <fagundes.daniel191@gmail.com>
2023-05-03 21:54:35 -0300

    end

--- mm.py
+++ mm.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 
 functions_list = [2, 3, 4, 5, 6, 7, 8]  # functions to be tested
 f_index = 0
-dims = [10]
+dims = [2, 10]
 d_index = 0
-budget_multiplier = 100 # 10000
-n_runs = 2 # 51
+budget_multiplier = 10000  # 10000
+n_runs = 51  # 51
+
 
 def alternate_dimensions():
     global d_index
@@ -28,7 +29,7 @@ if __name__ == '__main__':
 
     for f_index in range(len(functions_list)):
         for d_index in range(len(dims)):
-            print("\nFunction: f", (functions_list[f_index] + 1))
+            print("\nFunction: f", (functions_list[f_index] + 1), "D = ", dims[d_index])
 
             size_problem = int(dims[d_index])
             problem = Problem(size_problem, 1)
@@ -36,28 +37,35 @@ if __name__ == '__main__':
             problem.function = test_fun
             problem.directions[:] = Problem.MINIMIZE
             algorithm = NSGAII
+            algorithm.population_size = 50
 
             solutions_list = []
             fx_values = []
-            f = functions.all_functions[functions_list[f_index]] # encontrando o endereco da funcao
+            f = functions.all_functions[functions_list[f_index]]  # encontrando o endereco da funcao
+
             for i in range(n_runs):
-                value = experiment(algorithm, problem, nfe=(dims[d_index] * budget_multiplier), seeds=1)
+                value = experiment(
+                    algorithm,
+                    problem,
+                    nfe=(dims[d_index] * budget_multiplier),
+                    seeds=1)
+
                 solutions_list.append(value.get("NSGAII").get("Problem")[0][i].variables)
-                m = np.array(solutions_list[i]).reshape(5, -1) # transformando a lista 1x5 em matriz 5x1, apenas um teste
-                fx_values.append(f(m)) # chamando funcao f com a matriz m
+                m = np.array(solutions_list[i]).reshape(-1, 2)  # transformando a lista para bi dimensional
+                fx_values.append(f(m))  # chamando funcao f com a matriz m
 
             maior = max(max(sublist) for sublist in fx_values)
             menor = min(min(sublist) for sublist in fx_values)
 
-            # mean = np.mean(fx_values, axis=0)
-            # median = np.median(fx_values, axis=0)
-            # std = np.std(fx_values, axis=0)
-            # print("D =" + str(dims[d_index]))
-            # print("media: ", mean)
-            # print("mediana: ", median)
-            # print("desvio padrao: ", std)
+            mean = np.mean(np.mean(fx_values, axis=0), axis=0)
+            median = np.median(np.median(fx_values, axis=0), axis=0)
+            std = np.std(np.std(fx_values, axis=0), axis=0)
+            print("D =" + str(dims[d_index]))
             print("maior: ", maior)
             print("menor: ", menor)
+            print("media: ", mean)
+            print("mediana: ", median)
+            print("desvio padrao: ", std)
 
             alternate_dimensions()
         print("\n")
